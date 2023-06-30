bba0fa6f481d572bb8c9e3da4a02aac08ea888bc
Devon <dmmartens91@gmail.com>
2023-06-30 06:14:53 -0400

    changes verbage and updates tests accurately

--- Bridge-Token-Hardhat/test/hrc20.test.js
+++ Bridge-Token-Hardhat/test/hrc20.test.js
@@ -35,33 +35,33 @@ describe("Testing the initial values to validate expected contract state", funct
   DEFAULT_ADMIN_ROLE = await BackedHRC20Contract.DEFAULT_ADMIN_ROLE();
   await BackedHRC20Contract.issueBackedToken(ContractDeployer, 900);
   });
-  it("Backed HRC20: have correct values for name & symbol", async () => {
+  it("Backed HRC20: name & symbol variables should match constructor input for the ERC20 token", async () => {
     //confirm they are eqaul to the value set in the constructor
     expect(await BackedHRC20Contract.name()).to.equal("HAVEN1");
     //confirm they are eqaul to the value set in the constructor
     expect(await BackedHRC20Contract.symbol()).to.equal("HRC20");
   });
-  it("Upon deployment tokens minted should equal totalSupply", async () => {
+  it("Backed HRC20: the amount of tokens minted should equal totalSupply", async () => {
     expect(await BackedHRC20Contract.totalSupply()).to.equal(900);
   });
-  it("initalize should only be called upon deployment", async () => {
+  it("Backed HRC20: initalize should only be called upon deployment and afterwards it should not be possible to call", async () => {
     await expectRevert(
       BackedHRC20Contract.initialize("HAVEN1", "HRC20", ContractDeployer, ContractDeployer),
       "Initializable: contract is already initialized"
     );
   });
 
-  it("Backed HRC20: the deposit function should mint the correct amount of tokens to the designated wallet", async () => {
+  it("Backed HRC20: the issueBackedToken function should mint the correct amount of tokens to the designated wallet assigned when the function is called", async () => {
     expect(await BackedHRC20Contract.balanceOf(ContractDeployer)).to.equal(900);
   });
-  it("Backed HRC20: the withdraw function should burn the correct amount of tokens from the designated wallet", async () => {
+  it("Backed HRC20: the redeemBackedTokenfunction should burn the correct amount of tokens from the designated wallet", async () => {
     await BackedHRC20Contract.redeemBackedToken(900);
     expect(await BackedHRC20Contract.balanceOf(ContractDeployer)).to.equal(0);
   });
-  it("Backed HRC20: the withdraw function should revert and give the error INSUFFICENT_BALANCE if a request is made to withdraw more than the balance", async () => {
+  it("Backed HRC20: the redeemBackedToken function should revert and give the error INSUFFICENT_BALANCE if a request is made to burn more tokens than the balance of the address", async () => {
     await expectRevert(BackedHRC20Contract.redeemBackedToken(ContractDeployer), `110`);
   });
-  it("Backed HRC20: function pause function should stop all deposits", async () => {
+  it("Backed HRC20: function pause function should stop all deposits from the issueBackedToken function", async () => {
     await BackedHRC20Contract.pause();
     await expectRevert(BackedHRC20Contract.issueBackedToken(Address2, 900), "Pausable: paused");
   });
@@ -77,7 +77,7 @@ describe("Testing the initial values to validate expected contract state", funct
     await BackedHRC20Contract.pause();
     await expectRevert(BackedHRC20Contract.transfer(ContractDeployer, 450), "Pausable: paused");
   });
-  it("only the OPERATOR_ROLE should be able to pause unpause   ", async () => {
+  it("Backed HRC20: only the OPERATOR_ROLE should be able to pause and unpause the contract", async () => {
     await expectRevert(
       Address2SignsBackedHRC20.pause(),
       `AccessControl: account ${Address2ErrorMessageForAccessControl} is missing role ${OPERATOR_ROLE}`
@@ -89,7 +89,7 @@ describe("Testing the initial values to validate expected contract state", funct
     );
     await BackedHRC20Contract.unpause();
   });
-  it("the operator role should be able to call pause", async () => {
+  it("Backed HRC20: the operator role should be able to call pause", async () => {
     await expectRevert(
       Address2SignsBackedHRC20.pause(),
       `AccessControl: account ${Address2ErrorMessageForAccessControl} is missing role ${OPERATOR_ROLE}`
@@ -97,7 +97,6 @@ describe("Testing the initial values to validate expected contract state", funct
     await BackedHRC20Contract.grantRole(OPERATOR_ROLE, Address2);
     await Address2SignsBackedHRC20.pause();
   });
-  //hi
   it("Backed HRC20: only the operator role should be able to call burnFrom ", async () => {
     await BackedHRC20Contract.issueBackedToken(ContractDeployer, 70);
     await expectRevert(
@@ -111,7 +110,7 @@ describe("Testing the initial values to validate expected contract state", funct
       `AccessControl: account ${Address2ErrorMessageForAccessControl} is missing role ${OPERATOR_ROLE}`
     );
   });
-  it("upgrades should only be allowed to be called by DEFAULT_ADMIN_ROLE", async function () {
+  it("Backed HRC20: upgrades should only be allowed to be called by DEFAULT_ADMIN_ROLE", async function () {
     const HRC20HasADifferentUpgrader = await upgrades.deployProxy(
       BackedHRC20Factory,
       ["HAVEN1", "HRC20", Address2, Address2],
@@ -123,7 +122,7 @@ describe("Testing the initial values to validate expected contract state", funct
       }),
       `AccessControl: account ${ContractDeployerErrorMessageForAccessControl} is missing role ${DEFAULT_ADMIN_ROLE}`
     );
-    const HRC20V2 = await upgrades.upgradeProxy(BackedHRC20Contract.address, BackedHRC20Factory, {
+    await upgrades.upgradeProxy(BackedHRC20Contract.address, BackedHRC20Factory, {
       kind: "uups",
     });
   });
@@ -136,7 +135,7 @@ describe("Testing the initial values to validate expected contract state", funct
     await Address2SignsBackedHRC20.redeemBackedToken(900);
     expect(await BackedHRC20Contract.balanceOf(Address2)).to.equal(0);
   });
-  it("Backed HRC20: the withdraw function should revert and give the error BALANCE_TOO_LOW if a request is made to withdraw more than the balance", async () => {
+  it("Backed HRC20: the redeemBackedTokenfunction should revert and give the error INSUFFICIENT_BALANCE if a request is made to withdraw more than the balance", async () => {
     await expectRevert(Address2SignsBackedHRC20.redeemBackedToken(Address2), `110`);
   });
   it("Backed HRC20: burnFrom function should remove tokens from a wallet", async () => {
@@ -144,13 +143,13 @@ describe("Testing the initial values to validate expected contract state", funct
     await BackedHRC20Contract.burnFrom(Address2, 900, "YOU_STOLE_THAT");
     expect(await BackedHRC20Contract.balanceOf(Address2)).to.equal(0);
   });
-  it("Backed HRC20: A wallet that is not a contract should not be allowed to be approved by the function increaseAllowance", async () => {
+  it("Backed HRC20: If a user request to approve another user to move tokens via increaseAllowance the function should revert", async () => {
     await expectRevert(BackedHRC20Contract.increaseAllowance(Address2, 8), "116");
     expect(await BackedHRC20Contract.allowance(ContractDeployer, Address2)).to.equal(0);
   });
-  it("Backed HRC20: A wallet address that is not a contract NOT be allowed to be approved by ERC20 function approve", async () => {
-    await expectRevert(BackedHRC20Contract.approve(Address2, 8), "116");
-    expect(await BackedHRC20Contract.allowance(ContractDeployer, Address2)).to.equal(0);
+  it("Backed HRC20: a contract should be allowed to be approved by ERC20 function approve", async () => {
+    await BackedHRC20Contract.approve(BackedHRC20Contract.address, 8);
+    expect(await BackedHRC20Contract.allowance(ContractDeployer, BackedHRC20Contract.address)).to.equal(8);
   });
   it("Backed HRC20: A contract should not be allowed to be approved by the function increaseAllowance", async () => {
     const BackedHRC20ContractAlternateAdminAndOperators = await upgrades.deployProxy(
