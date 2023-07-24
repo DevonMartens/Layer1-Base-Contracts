09b3594f8aeab533b22ffa4a75fa9c41053e01b9
Devon <dmmartens91@gmail.com>
2023-07-24 12:34:58 +0200

    removes more redudancies

--- Prover-Test-Folder/contracts/IUserInformationPreventsOnExpiry.sol
+++ Prover-Test-Folder/contracts/IUserInformationPreventsOnExpiry.sol
@@ -6,30 +6,28 @@ import "./IRoleVerification.sol";
 
 
 /**
-@title IUserInformationPreventsOnExpiry
-@notice Interface for accessing user information and enforcing 
+* @title IUserInformationPreventsOnExpiry
+* @notice Interface for accessing user information and enforcing 
 * in-date identity documents for access to functions.
+* @dev All functions revert when the target account's identity has expired.
 */
 
 interface IUserInformationPreventsOnExpiry is IRoleVerification {
 
     /*
     @notice `getUserAccountCountryCodePreventOnExpiry` retrieves the country code from the user's account.
-    @dev Reverts if the target account has expired.
     @param account Address of the target user account.
     */
     function getUserAccountCountryCodePreventOnExpiry(address account) external view returns (string memory);
 
     /**
     @notice `getUserAccountLevelPreventOnExpiry` retrieves the verification level from the user's account.
-    @dev Reverts if the target account has expired.
     @param account Address of the target user account.
     */
     function getUserAccountLevelPreventOnExpiry(address account) external view returns (uint8);
 
     /**
     @notice `getUserAccountTypePreventOnExpiry` retrieves the account type from the user's account.
-    @dev Reverts if the target account has expired.
     @param account Address of the target user account.
     */
     function getUserAccountTypePreventOnExpiry(address account) external view returns (uint8);
@@ -38,7 +36,6 @@ interface IUserInformationPreventsOnExpiry is IRoleVerification {
      * @notice `getUserCompetencyRatingPreventOnExpiry` gets the competency rating a user
      * earned testing but access to it is prevented if the identity has expired.
      * @param account The address of the target user account.
-     * @dev This function reverts if the target account's identity has expired.
      */
      function getUserAccountCompetencyRatingPreventOnExpiry(
         address account
